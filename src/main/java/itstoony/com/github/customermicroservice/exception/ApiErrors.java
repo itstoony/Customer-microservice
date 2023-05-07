f5ac35395af7a6f69952e8579aeecc78e6ad47e6
Tony <toonyrenner@gmail.com>
2023-05-07 12:10:24 -0300

    Implementing ControllerAdvice.

---
+++ src/main/java/itstoony/com/github/customermicroservice/exception/ApiErrors.java
@@ -0,0 +1,25 @@
+package itstoony.com.github.customermicroservice.exception;
+
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public class ApiErrors {
+
+    private final List<String> errors;
+
+    public ApiErrors(BindingResult bindingResult) {
+        this.errors = new ArrayList<>();
+        bindingResult.getAllErrors().forEach(e -> this.errors.add(e.getDefaultMessage()));
+    }
+
+    public ApiErrors(ResponseStatusException ex) {
+        this.errors = Collections.singletonList(ex.getReason());
+    }
+
+}
